02a2c8de6b0610d5b977d0bbf9cd6b043136bb50
Steeve Lefort <steeve@lefort-software.com>
2025-07-30 12:28:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Développez le back-end en utilisant Java et Spring
+
+Running:
+
+```sh
+$ DB_USERNAME=xxx DB_PASSWORD=yyy mvn spring-boot:run
+```
+
+Where xxx is the database username and yyy is the password
---
+++ src/main/resources/data.sql
@@ -0,0 +1,37 @@
+CREATE TABLE IF NOT EXISTS `USERS` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `email` varchar(255),
+  `name` varchar(255),
+  `password` varchar(255),
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
+
+CREATE TABLE IF NOT EXISTS `RENTALS` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `name` varchar(255),
+  `surface` numeric,
+  `price` numeric,
+  `picture` varchar(255),
+  `description` varchar(2000),
+  `owner_id` integer NOT NULL,
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
+
+CREATE TABLE IF NOT EXISTS `MESSAGES` (
+  `id` integer PRIMARY KEY AUTO_INCREMENT,
+  `rental_id` integer,
+  `user_id` integer,
+  `message` varchar(2000),
+  `created_at` timestamp,
+  `updated_at` timestamp
+);
+
+CREATE UNIQUE INDEX IF NOT EXISTS `USERS_index` ON `USERS` (`email`);
+
+ALTER TABLE `RENTALS` ADD FOREIGN KEY (`owner_id`) REFERENCES `USERS` (`id`);
+
+ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`user_id`) REFERENCES `USERS` (`id`);
+
+ALTER TABLE `MESSAGES` ADD FOREIGN KEY (`rental_id`) REFERENCES `RENTALS` (`id`);
