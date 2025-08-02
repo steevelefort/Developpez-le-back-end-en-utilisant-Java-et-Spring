646dd085185564cead91a3e9dd5eceae0d9e3ea0
Steeve Lefort <steeve@lefort-software.com>
2025-08-02 12:06:41 +0200

    feature(rental): working /api/rentals/{id} to see a rental by id

--- src/main/java/com/openclassrooms/controller/RentalController.java
+++ src/main/java/com/openclassrooms/controller/RentalController.java
@@ -6,9 +6,11 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.openclassrooms.dto.response.ErrorResponse;
 import com.openclassrooms.dto.response.RentalListResponse;
 import com.openclassrooms.dto.response.RentalResponse;
 import com.openclassrooms.model.Rental;
@@ -30,6 +32,16 @@ public class RentalController {
     Iterable<Rental> rentals = rentalService.getRentals();
     RentalListResponse response = new RentalListResponse(rentals);
     return ResponseEntity.ok(response);
+  }
+
+  @GetMapping(value = "/rentals/{id}", produces = "application/json")
+  public ResponseEntity<?> rental(@PathVariable Integer id) {
+    try {
+      RentalResponse response = rentalService.getRental(id);
+      return ResponseEntity.ok(response);
+    } catch (Exception e) {
+      return ResponseEntity.badRequest().body(new ErrorResponse(e.getMessage()));
+    }
   }
 
   // public ResponseEntity<?> rentals(@AuthenticationPrincipal Jwt jwt) {
--- src/main/java/com/openclassrooms/service/RentalService.java
+++ src/main/java/com/openclassrooms/service/RentalService.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.openclassrooms.dto.response.RentalResponse;
 import com.openclassrooms.model.Rental;
 import com.openclassrooms.repository.RentalRepository;
 
@@ -14,8 +15,9 @@ public class RentalService {
   @Autowired
   private RentalRepository rentalRepository;
 
-  public Optional<Rental> getRental(final Integer id) {
-    return rentalRepository.findById(id);
+  public RentalResponse getRental(final Integer id) {
+    Rental rental = rentalRepository.findById(id).orElseThrow(() -> new RuntimeException("Location introuvable"));
+    return new RentalResponse(rental); 
   }
 
   public Iterable<Rental> getRentals() {
